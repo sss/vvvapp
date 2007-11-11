7ed498c8030a6b2a1bf480f0d3e936950e2021e6
fsenore <fsenore@b0e3558a-1426-0410-83de-bf875247a1ac>
2007-11-11 09:40:01 +0000

    Added the GPL header to some files.
    git-svn-id: https://vvvapp.svn.sourceforge.net/svnroot/vvvapp/trunk@145 b0e3558a-1426-0410-83de-bf875247a1ac

--- src/settings.h
+++ src/settings.h
@@ -1,13 +1,24 @@
-/////////////////////////////////////////////////////////////////////////////
-// Name:        settings.h
-// Purpose:     
-// Author:      
-// Modified by: 
-// Created:     10/11/2007 09:58:37
-// RCS-ID:      
-// Copyright:   
-// Licence:     
-/////////////////////////////////////////////////////////////////////////////
+/*
+	This file is part of VVV (Virtual Volumes View)
+
+	Copyright (C) 2007, the VVV Development team
+
+	Author: Fulvio Senore
+
+    VVV is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    VVV is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with VVV; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 
 #ifndef _SETTINGS_H_
 #define _SETTINGS_H_
